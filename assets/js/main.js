c5b017bc6dc5dafcbbb6f30be4ea779549cc0b65
Gabriel Severo <gabriiellss15@gmail.com>
2023-09-06 14:47:20 -0400

    Loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,2 @@
 
-// Detecta quando a página é rolada
-// window.addEventListener('scroll', function() {
-//     // Altura em que você deseja mostrar o conteúdo (ajuste conforme necessário)
-//     var showAtHeight = 200; 
 
-//     // Verifica se a posição vertical da página é maior ou igual à altura desejada
-//     if (window.scrollY >= showAtHeight) {
-//         // Adiciona a classe para mostrar o conteúdo
-//         document.querySelector('.content').classList.add('show-content');
-//     }
-// });
